ffbb8dbd593d6014190b88cebdd377f0a258cef7
くしゃみの人 <contact@kshun.info>
2020-09-27 21:48:17 +0900

    add a new theme

---
+++ lib/theme/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+const Color oxfordBlue = Color(0xFF0B132B);
+const Color spaceCadet = Color(0xFF1C2541);
+const Color independence = Color(0xFF3A506B);
+const Color maximumBlueGreen = Color(0xFF5BC0BE);
+const Color powderBlue = Color(0xFFADE0DF);
+const Color spaceCadetLight = Color(0xFFD4DAED);
+
+const Color backgroundWhite = Colors.white;
+const Color surfaceWhite = Color(0xFFFFFBFA);
+
+const Color disabledGrey = Colors.grey;
+const Color errorRed = Colors.redAccent;
---
+++ lib/theme/theme.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'color.dart';
+
+
+final ThemeData mainTheme = _buildTheme();
+
+ThemeData _buildTheme() {
+  final base = ThemeData.light();
+  return base.copyWith(
+    appBarTheme: const AppBarTheme(brightness: Brightness.light, elevation: 0),
+    colorScheme: _mainColorScheme,
+    primaryColor: spaceCadet,
+    canvasColor: backgroundWhite,
+    accentColor: oxfordBlue,
+    buttonColor: maximumBlueGreen,
+    scaffoldBackgroundColor: backgroundWhite,
+    cardColor: backgroundWhite,
+    disabledColor: disabledGrey,
+    buttonTheme: const ButtonThemeData(
+      colorScheme: _mainColorScheme,
+      textTheme: ButtonTextTheme.normal,
+    ),
+    primaryIconTheme: _customIconTheme(base.iconTheme),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: oxfordBlue,
+        ),
+      ),
+      contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+    ),
+    textTheme: base.textTheme,
+    primaryTextTheme: base.primaryTextTheme,
+    accentTextTheme: base.accentTextTheme,
+    iconTheme: _customIconTheme(base.iconTheme),
+  );
+}
+
+IconThemeData _customIconTheme(IconThemeData original) {
+  return original.copyWith(color: oxfordBlue);
+}
+
+const ColorScheme _mainColorScheme = ColorScheme(
+  primary: spaceCadet,
+  primaryVariant: spaceCadetLight,
+  secondary: independence,
+  secondaryVariant: maximumBlueGreen,
+  surface: surfaceWhite,
+  background: backgroundWhite,
+  error: errorRed,
+  onPrimary: surfaceWhite,
+  onSecondary: oxfordBlue,
+  onSurface: oxfordBlue,
+  onBackground: oxfordBlue,
+  onError: surfaceWhite,
+  brightness: Brightness.light,
+);
