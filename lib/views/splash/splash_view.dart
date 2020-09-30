a2011b7c70f76a219c2e892adf2e287d1dae70df
くしゃみの人 <contact@kshun.info>
2020-09-30 20:48:37 +0900

    refactor 20200930

--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -1,73 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../common_items/widgets/opening_template_widget.dart';
+
 class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).backgroundColor,
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                  flex: 2,
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 50.0,
-                          backgroundColor: Theme.of(context).canvasColor,
-                          child: Icon(
-                            Icons.collections_bookmark_sharp,
-                            size: 50.0,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 30.0),
-                        ),
-                        Text(
-                          'Pre KC Cup App',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24.0,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(),
-                    Padding(padding: EdgeInsets.only(top: 10.0)),
-                    Text(
-                      'Loading ...',
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ],
+    return OpeningTemplateWidget(<Widget>[
+      CircularProgressIndicator(),
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Text(
+        'Loading ...',
+        softWrap: true,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+          color: Theme.of(context).primaryColor,
+        ),
       ),
-    );
+    ]);
   }
 }
