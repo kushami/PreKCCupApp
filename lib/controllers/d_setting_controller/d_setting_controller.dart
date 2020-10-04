c1434a0522f7eff41978bb3fd8b2103d599b66d0
くしゃみの人 <contact@kshun.info>
2020-10-04 11:02:40 +0900

    ...

--- lib/controllers/d_setting_controller/d_setting_controller.dart
+++ lib/controllers/d_setting_controller/d_setting_controller.dart
@@ -1,25 +1,38 @@
 import 'package:state_notifier/state_notifier.dart';
 
 import '../../models/duelist_setting/duelist_setting.dart';
+import '../../repositories/value_persistence_repository/value_persistence_repo.dart';
 
 class DSettingController extends StateNotifier<DuelistSetting>
     with LocatorMixin {
   DSettingController() : super(const DuelistSetting());
 
+  ValuePersistenceRepository get _sharedPrefs => read();
+
   @override
   void initState() {
     super.initState();
+
+    // TODO: PKC 参加してる人はいい感じに Number ふられるようにする
+    // 登録されていないと 0.
     state = state.copyWith(
-      myDeckID: 'testDeckID',
-      mySkillID: 'testSkillID',
+      pkcNumber: 0,
+      playerNumber: 0,
     );
+
+    _sharedPrefs.getString('myDeckID').then(
+        (deckID) => state = state.copyWith(myDeckID: deckID ?? 'No Selected'));
+    _sharedPrefs.getString('mySkillID').then((skillID) =>
+        state = state.copyWith(mySkillID: skillID ?? 'No Selected'));
   }
 
-  void setMyDeck(String deckID) {
+  void setMyDeck(String deckID) async {
     state = state.copyWith(myDeckID: deckID);
+    await _sharedPrefs.setString('myDeckID', deckID);
   }
 
-  void setMySkill(String skillID) {
+  void setMySkill(String skillID) async {
     state = state.copyWith(mySkillID: skillID);
+    await _sharedPrefs.setString('mySkillID', skillID);
   }
 }
--- lib/views/root_view.dart
+++ lib/views/root_view.dart
@@ -8,15 +8,18 @@ import 'home/home_view.dart';
 import '../models/auth_state/auth_state.dart';
 import '../models/duelist_setting/duelist_setting.dart';
 import '../controllers/d_setting_controller/d_setting_controller.dart';
+import '../repositories/value_persistence_repository/value_persistence_repo.dart';
 
 class RootView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (context.select<AuthState, User>((s) => s.user) == null)
         ? SigninView()
-        : StateNotifierProvider<DSettingController, DuelistSetting>(
-            create: (context) => DSettingController(),
-            child: HomeView(),
-          );
+        : Provider(
+            create: (_) => ValuePersistenceRepository(),
+            child: StateNotifierProvider<DSettingController, DuelistSetting>(
+              create: (context) => DSettingController(),
+              child: HomeView(),
+            ));
   }
 }
